3fa6129fddaf63f30004733ca92fcd6d1884f120
Gayane25 <gayabennu@gmail.com>
2021-12-04 23:08:27 +0400

    seperate arrays for even and odd numbers

--- ex1_oddOrEven.js
+++ ex1_oddOrEven.js
@@ -1,1 +1,15 @@
 //Given an array of numbers. Write a function to separate odd and even numbers in different arrays.
+
+function getSeperator(array) {
+  let arrEven = [];
+  let arrOdd = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      arrEven.push(array[i]);
+    } else {
+      arrOdd.push(array[i]);
+    }
+  }
+  console.log(arrEven, arrOdd);
+}
+getSeperator([45, 12, 3, 6, 17, 0]);
