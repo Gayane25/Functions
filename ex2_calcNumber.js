787a154278458c05a7d1741961640b0d52cdcff3
Gayane25 <gayabennu@gmail.com>
2021-12-05 17:27:17 +0400

    function written with reduce method

--- ex2_calcNumber.js
+++ ex2_calcNumber.js
@@ -1,3 +1,27 @@
 'use strict';
 /*Write a function that calculates sum, difference, multiplication and division between given array
 elements depending on passed operation symbol. Write appropriate function for each operation.*/
+
+const calcNumb = (array, operator) => {
+  if (operator === '+') {
+    return array.reduce((prev, next) => {
+      return prev + next;
+    });
+  }
+  if (operator === '-') {
+    return array.reduce((prev, next) => {
+      return prev - next;
+    });
+  }
+  if (operator === '*') {
+    return array.reduce((prev, next) => {
+      return prev * next;
+    });
+  }
+  if (operator === '/') {
+    return array.reduce((prev, next) => {
+      return prev / next;
+    });
+  }
+};
+console.log(calcNumb([15, 10, 3, 1], '/'));
